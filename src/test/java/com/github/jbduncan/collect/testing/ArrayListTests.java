93a4238e725486e69a01365e4ca6d9c4e4869895
Jonathan Bluett-Duncan <jbluettduncan@gmail.com>
2018-03-12 01:21:14 +0000

    Use copy of elements array in ArrayListTests
    This will help to ensure that the original elements array is never
    mutated accidentally.

--- src/test/java/com/github/jbduncan/collect/testing/ArrayListTests.java
+++ src/test/java/com/github/jbduncan/collect/testing/ArrayListTests.java
@@ -1,13 +1,16 @@
 package com.github.jbduncan.collect.testing;
 
+import static java.util.Arrays.asList;
+import static java.util.Arrays.copyOf;
+
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Set;
 
 class ArrayListTests implements ListContract<String> {
   @Override
   public TestListGenerator<String> generator() {
-    return (TestStringListGenerator) elements -> new ArrayList<>(Arrays.asList(elements));
+    return (TestStringListGenerator)
+        elements -> new ArrayList<>(asList(copyOf(elements, elements.length)));
   }
 
   @Override
