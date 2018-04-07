e49f1c0068f0cc4a20ee504229ce9474701852d1
Jonathan Bluett-Duncan <jbluettduncan@gmail.com>
2018-04-07 19:09:22 +0100

    Update dependency & Gradle versions; build script cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,14 +3,16 @@ plugins {
     eclipse
     idea
 
-    id("com.github.ben-manes.versions") version("0.17.0")
     id("com.diffplug.gradle.spotless") version("3.10.0")
+    id("com.github.ben-manes.versions") version("0.17.0")
     // TODO: Consider swapping out for
     // https://github.com/tbroyer/gradle-errorprone-javacplugin-plugin
-    id("net.ltgt.errorprone") version ("0.0.13")
+    id("net.ltgt.errorprone") version("0.0.13")
+
     // TODO: Add other static analysis and formal verification tools
 }
 
+// Configuration for Java
 group = "com.github.jbduncan"
 version = "1.0-SNAPSHOT"
 
@@ -24,13 +26,6 @@ tasks.withType<JavaCompile> {
     options.compilerArgs.addAll(listOf("-Xlint:all", "-Werror"))
 }
 
-val compileTestJava by tasks.getting(JavaCompile::class) {
-    // error-prone options
-    options.compilerArgs.addAll(
-            // Produces false positives against JUnit Platform @Nested tests
-            listOf("-Xep:ClassCanBeStatic:OFF"))
-}
-
 tasks.withType<Test> {
     useJUnitPlatform()
 }
@@ -39,10 +34,10 @@ repositories {
     jcenter()
 }
 
-val junit5Version by project.properties
-val guavaVersion by project.properties
-val truthVersion by project.properties
-val errorProneVersion by project.properties
+val junit5Version: String by project
+val guavaVersion: String by project
+val truthVersion: String by project
+val errorProneVersion: String by project
 
 dependencies {
     implementation("org.junit.jupiter:junit-jupiter-api:$junit5Version")
@@ -52,20 +47,43 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junit5Version")
     testImplementation("org.junit.jupiter:junit-jupiter-params:$junit5Version")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junit5Version")
+}
 
+// Configuration for error-prone: https://github.com/tbroyer/gradle-errorprone-plugin
+dependencies {
     errorprone("com.google.errorprone:error_prone_core:$errorProneVersion")
     compileOnly("com.google.errorprone:error_prone_annotations:$errorProneVersion")
 }
 
-val googleJavaFormatVersion by project.properties
-val ktlintVersion by project.properties
+val compileTestJava by tasks.getting(JavaCompile::class) {
+    // error-prone options
+    options.compilerArgs.addAll(
+            // Produces false positives against JUnit Platform @Nested tests
+            listOf("-Xep:ClassCanBeStatic:OFF"))
+}
+
+// Configuration for Spotless: https://github.com/diffplug/spotless
+
+// This explicitly-named "dependency configuration" is declared and populated with the Maven
+// co-ordinates of various code formatters in the "dependencies" block below.
+// This is done so that when `./gradlew dependencyUpdates` is executed, gradle-versions-plugin can
+// then find the latest versions of all the formatters used by Spotless.
+val spotless by configurations.creating
+
+val googleJavaFormatVersion: String by project
+val ktlintVersion: String by project
+
+dependencies {
+    spotless("com.google.googlejavaformat:google-java-format:$googleJavaFormatVersion")
+    spotless("com.github.shyiko:ktlint:$ktlintVersion")
+}
 
 spotless {
     java {
-        googleJavaFormat("$googleJavaFormatVersion")
+        googleJavaFormat(googleJavaFormatVersion)
     }
     kotlinGradle {
-        ktlint("$ktlintVersion")
+        ktlint(ktlintVersion)
         endWithNewline()
     }
     format("misc") {
