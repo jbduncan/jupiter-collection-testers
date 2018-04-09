ee2972ee90f1830c59bd674a0ea1da7760bd7f24
Jonathan Bluett-Duncan <jbluettduncan@gmail.com>
2018-04-09 03:13:29 +0100

    Add ListContract tests for Collections.(empty|singleton)List()

--- src/test/java/com/github/jbduncan/collect/testing/ListContractTests.java
+++ src/test/java/com/github/jbduncan/collect/testing/ListContractTests.java
@@ -8,6 +8,7 @@ import com.google.common.collect.Streams;
 import com.google.common.graph.SuccessorsFunction;
 import com.google.common.graph.Traverser;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -27,7 +28,7 @@ class ListContractTests {
     private final List<String> generatedList = contract.generator().create(ELEMENTS.toArray());
 
     @Test
-    void theClassOfTheGeneratedListIsArrayList() {
+    void theClassOfTheGeneratedListIsAsExpected() {
       assertThat(generatedList.getClass()).isEqualTo(ArrayList.class);
     }
 
@@ -132,6 +133,123 @@ class ListContractTests {
     }
   }
 
+  @Nested
+  class ForCollectionsEmptyList {
+    private final ListContract<String> contract = new CollectionsEmptyListTests();
+    private final List<String> generatedList = contract.generator().create(ELEMENTS.toArray());
+
+    @Test
+    void theClassOfTheGeneratedListIsAsExpected() {
+      assertThat(generatedList.getClass()).isEqualTo(Collections.EMPTY_LIST.getClass());
+    }
+
+    @Test
+    void theGeneratedListIsEmpty() {
+      assertThat(generatedList).isEmpty();
+    }
+
+    @Test
+    void theExpandedFeaturesAreAsExpected() {
+      assertThat(contract.features())
+          .containsExactly(CollectionFeature.SERIALIZABLE, CollectionSize.SUPPORTS_ZERO);
+    }
+
+    @Test
+    void theAddTestFactoryHasTheExpectedStructure() {
+      assertExpectedStructure(
+          contract::add,
+          /* expectedDynamicContainerNames = */ ImmutableList.of(
+              "Does not support List.add(E)", "Does not support List.add(E) with null element"),
+          /* expectedDynamicTestNames = */ ImmutableList.of(
+              "Does not support List.add(E) with new element: size: 0, elements: []",
+              "Does not support List.add(E) with new null element: size: 0, elements: []"));
+    }
+
+    @Test
+    void theAddWithIndexTestFactoryHasTheExpectedStructure() {
+      assertExpectedStructure(
+          contract::addWithIndex,
+          /* expectedDynamicContainerNames = */ ImmutableList.of(
+              "Does not support List.add(int, E)",
+              "Does not support List.add(int, E) with null element"),
+          /* expectedDynamicTestNames = */ ImmutableList.of(
+              "Does not support List.add(0, E) with new element: size: 0, elements: []",
+              "Does not support List.add(size(), E) with new element: size: 0, elements: []",
+              "Does not support List.add(middleIndex(), E) with new element: size: 0, elements: []",
+              "Does not support List.add(0, E) with new null element: size: 0, elements: []",
+              "Does not support List.add(size(), E) with new null element: size: 0, elements: []",
+              "Does not support List.add(middleIndex(), E) with new null element: "
+                  + "size: 0, elements: []"));
+    }
+  }
+
+  @Nested
+  class ForCollectionsSingletonList {
+    private final ListContract<String> contract = new CollectionsSingletonListTests();
+    private final List<String> generatedList = contract.generator().create(ELEMENTS.toArray());
+
+    @Test
+    void theClassOfTheGeneratedListIsAsExpected() {
+      assertThat(generatedList.getClass()).isEqualTo(Collections.singletonList(null).getClass());
+    }
+
+    @Test
+    void theGeneratedListIsEmpty() {
+      assertThat(generatedList).containsExactly(ELEMENTS.get(0));
+    }
+
+    @Test
+    void theExpandedFeaturesAreAsExpected() {
+      assertThat(contract.features())
+          .containsExactly(
+              CollectionFeature.SERIALIZABLE,
+              CollectionFeature.ALLOWS_NULL_VALUES,
+              CollectionFeature.ALLOWS_NULL_QUERIES,
+              CollectionSize.SUPPORTS_ONE);
+    }
+
+    @Test
+    void theAddTestFactoryHasTheExpectedStructure() {
+      assertExpectedStructure(
+          contract::add,
+          /* expectedDynamicContainerNames = */ ImmutableList.of(
+              "Does not support List.add(E)", "Does not support List.add(E) with null element"),
+          /* expectedDynamicTestNames = */ ImmutableList.of(
+              "Does not support List.add(E) with new element: size: 1, elements: [a]",
+              "Does not support List.add(E) with existing element: size: 1, elements: [a]",
+              "Does not support List.add(E) with new null element: size: 1, elements: [a]",
+              "Does not support List.add(E) with existing null element: size: 1, elements: [null]"));
+    }
+
+    @Test
+    void theAddWithIndexTestFactoryHasTheExpectedStructure() {
+      assertExpectedStructure(
+          contract::addWithIndex,
+          /* expectedDynamicContainerNames = */ ImmutableList.of(
+              "Does not support List.add(int, E)",
+              "Does not support List.add(int, E) with null element"),
+          /* expectedDynamicTestNames = */ ImmutableList.of(
+              "Does not support List.add(0, E) with new element: size: 1, elements: [a]",
+              "Does not support List.add(0, E) with existing element: size: 1, elements: [a]",
+              "Does not support List.add(size(), E) with new element: size: 1, elements: [a]",
+              "Does not support List.add(size(), E) with existing element: size: 1, elements: [a]",
+              "Does not support List.add(middleIndex(), E) with new element: "
+                  + "size: 1, elements: [a]",
+              "Does not support List.add(middleIndex(), E) with existing element: "
+                  + "size: 1, elements: [a]",
+              "Does not support List.add(0, E) with new null element: size: 1, elements: [a]",
+              "Does not support List.add(0, E) with existing null element: "
+                  + "size: 1, elements: [null]",
+              "Does not support List.add(size(), E) with new null element: size: 1, elements: [a]",
+              "Does not support List.add(size(), E) with existing null element: "
+                  + "size: 1, elements: [null]",
+              "Does not support List.add(middleIndex(), E) with new null element: "
+                  + "size: 1, elements: [a]",
+              "Does not support List.add(middleIndex(), E) with existing null element: "
+                  + "size: 1, elements: [null]"));
+    }
+  }
+
   private void assertExpectedStructure(
       Supplier<Iterable<DynamicNode>> dynamicNodesSupplier,
       Iterable<String> expectedDynamicContainerNames,
@@ -171,6 +289,4 @@ class ListContractTests {
         .map(d -> (DynamicTest) d)
         .collect(toImmutableList());
   }
-
-  // TODO: Test against Collections.emptyList() and Collections.singletonList()
 }
