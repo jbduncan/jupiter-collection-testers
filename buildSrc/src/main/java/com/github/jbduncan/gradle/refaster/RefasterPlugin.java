c4b0cc57487c31b63a3611c7b84a135127ee064f
Jonathan Bluett-Duncan <jbluettduncan@gmail.com>
2018-06-19 22:57:13 +0100

    Teach Refaster plugin to work with Refaster 2.1.2+
    See: https://groups.google.com/forum/?utm_medium=email&utm_source=footer#!topic/error-prone-discuss/Ha2gYHER9ik

--- buildSrc/src/main/java/com/github/jbduncan/gradle/refaster/RefasterPlugin.java
+++ buildSrc/src/main/java/com/github/jbduncan/gradle/refaster/RefasterPlugin.java
@@ -1,14 +1,12 @@
 package com.github.jbduncan.gradle.refaster;
 
-import static com.github.jbduncan.gradle.refaster.Constants.DEFAULT_ERROR_PRONE_VERSION;
-import static com.github.jbduncan.gradle.refaster.Constants.DEFAULT_REFASTER_SOURCE_SET_DIR;
-import static com.github.jbduncan.gradle.refaster.Constants.ERROR_PRONE_CORE_MAVEN_COORDINATES_FORMAT;
 import static com.github.jbduncan.gradle.refaster.Constants.REFASTER_MAVEN_COORDINATES_FORMAT;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.List;
+import net.ltgt.gradle.errorprone.ErrorProneBasePlugin;
 import net.ltgt.gradle.errorprone.ErrorProneToolChain;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.tools.ant.taskdefs.condition.Os;
@@ -18,14 +16,19 @@ import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.Task;
 import org.gradle.api.artifacts.Configuration;
+import org.gradle.api.file.FileCollection;
+import org.gradle.api.file.FileTree;
 import org.gradle.api.file.SourceDirectorySet;
 import org.gradle.api.plugins.JavaBasePlugin;
 import org.gradle.api.plugins.JavaPluginConvention;
 import org.gradle.api.tasks.SourceSet;
 import org.gradle.api.tasks.compile.JavaCompile;
 
-// TODO: Consider allowing the user to pass in a graph that describes a partial ordering in which
-// the Refaster templates must be executed.
+// TODO: Consider allowing the user to pass in a graph describing a partial ordering in which the
+// Refaster templates should be executed.
+// TODO: Consider migrating to Kotlin.
+// TODO: Consider using Task Configuration Avoidance
+// (https://docs.gradle.org/4.9-rc-1/userguide/task_configuration_avoidance.html)
 public class RefasterPlugin implements Plugin<Project> {
   @Override
   public void apply(Project project) {
@@ -33,22 +36,14 @@ public class RefasterPlugin implements Plugin<Project> {
     int javaVersion = Integer.parseInt(JavaVersion.current().getMajorVersion());
     if (javaVersion != 8) {
       throw new GradleException(
-          "Plugin of type " + getClass() + " requires Java 8. "
-              + "Currently executing with Java "
-              + javaVersion
-              + ".");
+          String.format(
+              "Plugin of type %s requires Java 8. Currently executing with Java %s.",
+              getClass(), javaVersion));
     }
 
-    // TODO: Consider wrapping this method call chain in project.afterEvaluate to wait until after
-    // net.ltgt.errorprone has potentially been applied, so that we can ensure that the
-    // refaster(Check|Apply) JavaCompile-based sub-tasks use custom error-prone toolchains (as they
-    // currently do) rather than the default toolchain that net.ltgt.errorprone would automatically
-    // apply.
-    // Alternatively, migrate away from sub-tasks implementing JavaCompile and use project.javaexec
-    // instead.
     project
         .getPluginManager()
-        .withPlugin("net.ltgt.errorprone-base", appliedPlugin -> applyInternal(project));
+        .withPlugin("net.ltgt.errorprone", appliedPlugin -> applyInternal(project));
   }
 
   private void applyInternal(Project project) {
@@ -63,47 +58,57 @@ public class RefasterPlugin implements Plugin<Project> {
                 sourceSet ->
                     sourceSet.java(
                         files -> {
-                          files.srcDir(DEFAULT_REFASTER_SOURCE_SET_DIR);
+                          files.srcDir("src/refaster/java");
                         }));
 
-    // TODO: Allow any error-prone version 2.1.0+ to be used
-    String errorProneVersion = DEFAULT_ERROR_PRONE_VERSION;
+    // Retrieve the on-filesytem location of the user-defined Refaster templates
+    SourceDirectorySet refasterTemplates = refasterSourceSet.getAllJava();
+
+    for (File refasterTemplateFile : refasterTemplates) {
+      if (refasterTemplateFile.toString().contains(" ")) {
+        throw new GradleException(
+            "Refaster cannot compile the given template because it has space(s) in its path: "
+                + refasterTemplateFile);
+      }
+    }
+
+    // Discover the error-prone version used by gradle-errorprone-plugin
+    // TODO: Document that this plugin only supports error-prone and Refaster versions 2.1.2+
+    String errorProneVersion =
+        project
+            .getConfigurations()
+            .getByName(ErrorProneBasePlugin.CONFIGURATION_NAME)
+            .getResolvedConfiguration()
+            .getFirstLevelModuleDependencies(
+                element -> element.getName().equals("error_prone_core"))
+            .iterator()
+            .next()
+            .getModuleVersion();
+    String refasterMavenCoords =
+        String.format(REFASTER_MAVEN_COORDINATES_FORMAT, errorProneVersion);
 
     // Set up the Refaster annotations dependency for the Refaster templates
     project
         .getDependencies()
-        .add(
-            refasterSourceSet.getImplementationConfigurationName(),
-            String.format(REFASTER_MAVEN_COORDINATES_FORMAT, errorProneVersion));
-
-    SourceDirectorySet refasterTemplates = refasterSourceSet.getAllJava();
+        .add(refasterSourceSet.getImplementationConfigurationName(), refasterMavenCoords);
 
     File refasterBuildDir = project.file(String.format("%s/refaster", project.getBuildDir()));
 
-    RefasterTemplateCompile compileRefasterTemplatesTask =
-        project
-            .getTasks()
-            .create(
-                "compileRefasterTemplates",
-                RefasterTemplateCompile.class,
-                t -> {
-                  t.setErrorProneVersion(errorProneVersion);
-                  t.setRefasterTemplates(refasterTemplates);
-                  t.setCompiledRefasterTemplatesDir(
-                      project.file(String.format("%s/templates", refasterBuildDir)));
-                  t.setCompiledRefasterClassesDir(
-                      project.file(String.format("%s/classes", refasterBuildDir)));
-                });
+    String refasterConfigurationName = "refaster";
+    Configuration refasterConfiguration =
+        project.getConfigurations().maybeCreate(refasterConfigurationName);
+    project.getDependencies().add(refasterConfigurationName, refasterMavenCoords);
 
-    // TODO: Consider redirecting all logging messages by the refaster(Check|Apply) sub-tasks to a
-    // log file in the buildDir, as in https://stackoverflow.com/a/27679230/2252930
+    File compiledRefasterTemplatesDir =
+        project.file(String.format("%s/templates", refasterBuildDir));
 
-    Set<JavaCompile> allJavaCompileTasksExceptRefaster =
-        new LinkedHashSet<>(
-            project
-                .getTasks()
-                .withType(JavaCompile.class)
-                .matching(t -> !t.getName().equals(refasterSourceSet.getCompileJavaTaskName())));
+    // Compile all Refaster templates
+    List<JavaCompile> allJavaCompileTasksExceptRefaster = new ArrayList<>();
+    for (JavaCompile javaCompile : project.getTasks().withType(JavaCompile.class)) {
+      if (!javaCompile.getName().contains("Refaster")) {
+        allJavaCompileTasksExceptRefaster.add(javaCompile);
+      }
+    }
 
     Task refasterCheckTask =
         project
@@ -125,50 +130,52 @@ public class RefasterPlugin implements Plugin<Project> {
                   j.setDescription("Applies Refaster refactorings to source code in-place.");
                 });
 
-    // TODO: When the restriction on the error-prone version to use is lifted, consider using the
-    // gradle-errorprone-plugin's errorprone configuration instead, e.g.
-    // "project.getConfigurations().getByName(ErrorProneBasePlugin.CONFIGURATION_NAME)..."
-    Configuration errorproneCoreConfiguration =
-        project
-            .getConfigurations()
-            .detachedConfiguration(
-                project
-                    .getDependencies()
-                    .create(
-                        String.format(
-                            ERROR_PRONE_CORE_MAVEN_COORDINATES_FORMAT,
-                            compileRefasterTemplatesTask.getErrorProneVersion())));
+    for (File refasterTemplateFile : refasterTemplates) {
+      String refasterTemplateName = FilenameUtils.removeExtension(refasterTemplateFile.getName());
 
-    for (File refasterTemplate : refasterTemplates) {
+      File compiledRefasterTemplateFile =
+          Utils.toCompiledRefasterTemplateFile(
+              refasterTemplateFile, project.getRootDir(), compiledRefasterTemplatesDir);
+
+      Task compileRefasterTemplateSubTask =
+          createCompileRefasterTemplateTask(
+              project,
+              refasterConfiguration,
+              refasterTemplateFile,
+              refasterTemplateName,
+              compiledRefasterTemplateFile);
+
+      // Use compiled Refaster templates to perform refactorings
+
+      // TODO: Consider redirecting all logging messages by the refaster(Check|Apply) sub-tasks to a
+      // log file in the buildDir, as in https://stackoverflow.com/a/27679230/2252930
+
       for (JavaCompile underlyingJavaCompileTask : allJavaCompileTasksExceptRefaster) {
-        File compiledRefasterTemplateFile =
-            Utils.toCompiledRefasterTemplateFile(
-                refasterTemplate,
-                project.getRootDir(),
-                compileRefasterTemplatesTask.getCompiledRefasterTemplatesDir());
-        String refasterTemplateName = FilenameUtils.removeExtension(refasterTemplate.getName());
+        String javaCompileTaskNameCapitalised =
+            Utils.capitalise(underlyingJavaCompileTask.getName());
+        FileCollection classpath = underlyingJavaCompileTask.getClasspath();
+        FileTree source = underlyingJavaCompileTask.getSource();
 
         JavaCompile refasterCheckSubTask =
             project
                 .getTasks()
                 .create(
                     String.format(
-                        "refaster%sCheckWith%s",
-                        capitalise(underlyingJavaCompileTask.getName()),
-                        refasterTemplateName),
+                        "refasterCheckFor%sWith%s",
+                        javaCompileTaskNameCapitalised, refasterTemplateName),
                     JavaCompile.class,
                     j -> {
-                      j.setClasspath(underlyingJavaCompileTask.getClasspath());
-                      j.setSource(underlyingJavaCompileTask.getSource());
-                      j.setDestinationDir(
-                          project.file(
-                              String.format(
-                                  "%s/check/%s/%s/classes",
-                                  refasterBuildDir,
-                                  refasterTemplateName,
-                                  underlyingJavaCompileTask.getName())));
+                      // TODO: See if it's possible to forgo including the classpath and in turn
+                      // see if it's possible to combine compileJava's and compileTestJava's sources
+                      // together in one refaster check sub-task (or to combine all Java files a-la
+                      // Spotless).
+                      // (https://github.com/google/error-prone/blob/master/examples/maven/refaster-based-cleanup/pom.xml
+                      // might be able to help here.)
+                      j.setClasspath(classpath);
+                      j.setSource(source);
+                      Utils.setDestinationDirToTaskTemporaryDir(j);
 
-                      j.setToolChain(new ErrorProneToolChain(errorproneCoreConfiguration));
+                      j.setToolChain(new ErrorProneToolChain(refasterConfiguration));
 
                       String nullDir = Os.isFamily(Os.FAMILY_WINDOWS) ? "nul" : "/dev/null";
                       j.getOptions()
@@ -178,11 +185,9 @@ public class RefasterPlugin implements Plugin<Project> {
                                   "-XepPatchChecks:refaster:" + compiledRefasterTemplateFile,
                                   "-XepPatchLocation:" + nullDir));
 
-                      // TODO: See if there is a way of declaring this input without using an
-                      // internal API.
                       j.getInputs().file(compiledRefasterTemplateFile);
                     });
-        refasterCheckSubTask.dependsOn(compileRefasterTemplatesTask);
+        refasterCheckSubTask.dependsOn(compileRefasterTemplateSubTask);
         underlyingJavaCompileTask.mustRunAfter(refasterCheckSubTask);
         refasterCheckTask.dependsOn(refasterCheckSubTask);
 
@@ -192,21 +197,20 @@ public class RefasterPlugin implements Plugin<Project> {
                 .create(
                     String.format(
                         "refaster%sApplyWith%s",
-                        capitalise(underlyingJavaCompileTask.getName()),
-                        refasterTemplateName),
+                        javaCompileTaskNameCapitalised, refasterTemplateName),
                     JavaCompile.class,
                     j -> {
-                      j.setClasspath(underlyingJavaCompileTask.getClasspath());
-                      j.setSource(underlyingJavaCompileTask.getSource());
-                      j.setDestinationDir(
-                          project.file(
-                              String.format(
-                                  "%s/apply/%s/%s/classes",
-                                  refasterBuildDir,
-                                  refasterTemplateName,
-                                  underlyingJavaCompileTask.getName())));
+                      // TODO: See if it's possible to forgo including the classpath and in turn
+                      // see if it's possible to combine compileJava's and compileTestJava's sources
+                      // together in one refaster check sub-task (or to combine all Java files a-la
+                      // Spotless).
+                      // (https://github.com/google/error-prone/blob/master/examples/maven/refaster-based-cleanup/pom.xml
+                      // might be able to help here.)
+                      j.setClasspath(classpath);
+                      j.setSource(source);
+                      Utils.setDestinationDirToTaskTemporaryDir(j);
 
-                      j.setToolChain(new ErrorProneToolChain(errorproneCoreConfiguration));
+                      j.setToolChain(new ErrorProneToolChain(refasterConfiguration));
 
                       j.getOptions()
                           .setCompilerArgs(
@@ -216,24 +220,55 @@ public class RefasterPlugin implements Plugin<Project> {
                                   "-XepPatchLocation:IN_PLACE"));
 
                       // TODO: This is a hack to disable UP-TO-DATE checking, as for some reason
-                      // it doesn't work properly for all refasterApply sub-tasks. Remove this hack
-                      // if the problem is fixed.
+                      // it doesn't seem to work properly for refasterApply. Remove this hack if the
+                      // problem is fixed.
                       j.getOutputs().upToDateWhen(x -> false);
                     });
-        refasterApplySubTask.dependsOn(compileRefasterTemplatesTask);
+        refasterApplySubTask.dependsOn(compileRefasterTemplateSubTask);
         underlyingJavaCompileTask.mustRunAfter(refasterApplySubTask);
         refasterApplyTask.dependsOn(refasterApplySubTask);
       }
     }
   }
 
-  private static String capitalise(String value) {
-    if (value.isEmpty()) {
-      return "";
-    }
-    StringBuilder builder = new StringBuilder();
-    builder.appendCodePoint(Character.toUpperCase(value.codePointAt(0)));
-    value.codePoints().skip(1).forEachOrdered(builder::appendCodePoint);
-    return builder.toString();
+  private Task createCompileRefasterTemplateTask(
+      Project project,
+      Configuration refasterConfiguration,
+      File refasterTemplateFile,
+      String refasterTemplateName,
+      File compiledRefasterTemplateFile) {
+
+    JavaCompile compileRefasterTemplateSubTask =
+        project
+            .getTasks()
+            .maybeCreate(
+                String.format("compileRefasterTemplateNamed%s", refasterTemplateName),
+                JavaCompile.class);
+    compileRefasterTemplateSubTask.setSource(refasterTemplateFile);
+    compileRefasterTemplateSubTask.setToolChain(new ErrorProneToolChain(refasterConfiguration));
+    compileRefasterTemplateSubTask.setClasspath(
+        compileRefasterTemplateSubTask.getClasspath() == null
+            ? refasterConfiguration
+            : compileRefasterTemplateSubTask.getClasspath().plus(refasterConfiguration));
+    compileRefasterTemplateSubTask
+        .getOptions()
+        .setAnnotationProcessorPath(
+            compileRefasterTemplateSubTask.getOptions().getAnnotationProcessorPath() == null
+                ? refasterConfiguration
+                : compileRefasterTemplateSubTask
+                    .getOptions()
+                    .getAnnotationProcessorPath()
+                    .plus(refasterConfiguration));
+    Utils.setDestinationDirToTaskTemporaryDir(compileRefasterTemplateSubTask);
+
+    compileRefasterTemplateSubTask
+        .getOptions()
+        .getCompilerArgs()
+        .add("-Xplugin:RefasterRuleCompiler --out " + compiledRefasterTemplateFile);
+
+    compileRefasterTemplateSubTask.getInputs().file(refasterTemplateFile);
+    compileRefasterTemplateSubTask.getOutputs().file(compiledRefasterTemplateFile);
+
+    return compileRefasterTemplateSubTask;
   }
 }
