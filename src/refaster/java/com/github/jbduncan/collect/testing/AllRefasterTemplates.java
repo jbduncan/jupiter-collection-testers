c2c187e368764218078b6e14fb1ba94e8f0c6af7
Jonathan Bluett-Duncan <jbluettduncan@gmail.com>
2018-08-18 23:29:12 +0100

    Merge all Refaster templates into one
    It seems that with the latest version of Refaster, it is now possible
    to do this without hitting inexplicable errors. So we take advantage of
    it to significantly improve the speed of the `refaster[Check|Apply]`
    tasks and in turn make `gradlew check` faster.

---
+++ src/refaster/java/com/github/jbduncan/collect/testing/AllRefasterTemplates.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2018 the Jupiter Collection Testers authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.jbduncan.collect.testing;
+
+import com.google.errorprone.refaster.annotation.AfterTemplate;
+import com.google.errorprone.refaster.annotation.AlsoNegation;
+import com.google.errorprone.refaster.annotation.BeforeTemplate;
+import com.google.errorprone.refaster.annotation.MayOptionallyUse;
+import com.google.errorprone.refaster.annotation.Placeholder;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Stream;
+
+class AllRefasterTemplates {
+  static class CollectionIsEmpty<E> {
+    @BeforeTemplate
+    boolean sizeEqualsZero(Collection<E> collection) {
+      return collection.size() == 0;
+    }
+
+    @AfterTemplate
+    @AlsoNegation
+    boolean optimizedMethod(Collection<E> collection) {
+      return collection.isEmpty();
+    }
+  }
+
+  abstract static class IfSetAdd<E> {
+    @Placeholder
+    abstract void doAfterAdd(@MayOptionallyUse E element);
+
+    @BeforeTemplate
+    void ifNotContainsThenAdd(Set<E> set, E elem) {
+      if (!set.contains(elem)) {
+        set.add(elem);
+        doAfterAdd(elem);
+      }
+    }
+
+    @AfterTemplate
+    void ifAdd(Set<E> set, E elem) {
+      if (set.add(elem)) {
+        doAfterAdd(elem);
+      }
+    }
+  }
+
+  static class SortedFirst<T> {
+    @BeforeTemplate
+    Optional<T> before(Stream<T> stream, Comparator<? super T> comparator) {
+      return stream.sorted(comparator).findFirst();
+    }
+
+    @AfterTemplate
+    Optional<T> after(Stream<T> stream, Comparator<? super T> comparator) {
+      return stream.min(comparator);
+    }
+  }
+
+  static class StringIsEmpty {
+    @BeforeTemplate
+    boolean equalsEmptyString(String string) {
+      return string.equals("");
+    }
+
+    @BeforeTemplate
+    boolean lengthEquals0(String string) {
+      return string.length() == 0;
+    }
+
+    @AfterTemplate
+    @AlsoNegation
+    boolean optimizedMethod(String string) {
+      return string.isEmpty();
+    }
+  }
+}
